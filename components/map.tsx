fceace07fb9c7ac079b688581ea2bf482d9fd11b
michael <Michaelturvey97@gmail.com>
2025-01-23 23:44:16 -0500

    moved line processing out of client

--- components/map.tsx
+++ components/map.tsx
@@ -1,22 +1,27 @@
 //TODO: Experiment with lines to show row of 4
-//TODO: Try with polygons instead of KML
 
 'use client';
 
-import {useState, useEffect, ReactNode} from 'react'
+import {useState, useEffect, ReactNode, ReactElement} from 'react'
 import Link from 'next/link'
-import { GoogleMap, useJsApiLoader, KmlLayer, Marker, InfoWindow, Polygon } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, KmlLayer, Marker, InfoWindow, Polygon, Polyline } from '@react-google-maps/api';
 import { Library } from '@googlemaps/js-api-loader';
+import { LineString, Position } from 'geojson';
 
 const libraries : Library[] = ['places', 'geometry'];
-
+type featureData = {
+	featureName : string,
+	coords: { lat: number; lng: number; }[]
+};
 interface mapProps {
 	regionData: string
+	regions?: featureData[]; //TODO: make this global
+	lines?: featureData[];
 }
 
-export default function Map({regionData}: mapProps) {
+export default function Map({regions, lines}: mapProps) {
 	const [position, setPosition] = useState(center);
-
+	
 	//Load the map
 	const { isLoaded } = useJsApiLoader({
 		id: 'google-map-script',
@@ -48,6 +53,21 @@ export default function Map({regionData}: mapProps) {
 		}
 	}, []);
 
+	//Render region polygons
+	const regionElements = regions?.map((region) => {
+		return <Polygon 
+		path = {region.coords}
+		key = {region.featureName}
+		/>
+	})
+
+	//Render region lines
+	const lineElements = lines?.map((line) => {
+		return <Polyline 
+		key = {line.featureName}
+		path = {line.coords}/>
+	})
+
 	//Info window
 	const regionWindow = <InfoWindow 
 			position = {infoWindowPos}
@@ -69,15 +89,15 @@ export default function Map({regionData}: mapProps) {
 	//Make the polygons
 	function makePolygons(regionData: string) {
 		//Parse the region data
-		const regionDataObj = JSON.parse(regionData);
+		const regionDataObj: GeoJSON.FeatureCollection = JSON.parse(regionData);
 
 		//Create the polygons
-		const regionPolygons: ReactNode[] = regionDataObj.features.map((region: any) => {
+		const regionPolygons: ReactNode[] = regionDataObj.features.map((region: GeoJSON.Feature) => {
 			//check if region is a polygon
-			if(region.geometry.type !== "Polygon") {
+			if(region.geometry.type !== "Polygon" || !region.properties) {
 				return null;
 			}
-			const regionName = region.properties.name;
+			const regionName: string = region.properties.Name;
 			//convert coords to latlong
 			const regionCoords = region.geometry.coordinates[0].map((coord: any) => {
 				return {lat: coord[1], lng: coord[0]}
@@ -86,9 +106,9 @@ export default function Map({regionData}: mapProps) {
 			<Polygon
 				path = {regionCoords}
 				key = {regionName}
-				onClick = {() => {
+				onClick = {(evt) => {
+					evt.latLng ? setInfoWindowPos(evt.latLng.toJSON()) : null;
 					setCurrentRegion(regionName);
-					setInfoWindowPos(regionCoords[0]);
 				}}
 			/>
 		)
@@ -96,25 +116,56 @@ export default function Map({regionData}: mapProps) {
 		return regionPolygons;
 	}
 
+	function makeLines(regionData: string) {
+		//Parse the region data
+		const regionDataObj: GeoJSON.FeatureCollection = JSON.parse(regionData);
+
+		//Create the lines
+		const regionLines: ReactNode[] = regionDataObj.features.map((line: GeoJSON.Feature) => {
+			//check if region is a polygon
+			if(line.geometry.type !== "LineString" || !line.properties) {
+				return null;
+			}
+			const lineName: string = line.properties.Name;
+			//convert coords to latlong
+			const lineCoords = line.geometry.coordinates.map((coord: Position) => {
+				const latlong = coord as number[];
+				return {lat: latlong[1], lng: latlong[0]}
+			})
+			return (
+			<Polyline
+				path = {lineCoords}
+				key = {lineName}
+				onClick = {(evt) => {
+					evt.latLng ? setInfoWindowPos(evt.latLng.toJSON()) : null;
+					setCurrentRegion(lineName);
+				}}
+			/>
+		)
+		})
+		return regionLines;
+	}
+
 	//Render the map or loading screen
 	return isLoaded ? (
-		<div id="map">
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={12}
-    >
-	{/* This does the montreal grid */}
-	
-	{makePolygons(regionData)}
+	<div id="map">
+		<GoogleMap
+		mapContainerStyle={containerStyle}
+		center={center}
+		zoom={12}
+		>
+		{/* This does the montreal grid */}
+		
+		{regionElements}
+		{lineElements}
 
-	{/* This is the info window popup */}
-	{currentRegion? regionWindow: null}
-	{/* This is the location marker */}
-	
-	{locationMarker}
-    </GoogleMap>
-        </div>
+		{/* This is the info window popup */}
+		{currentRegion? regionWindow: null}
+		{/* This is the location marker */}
+		
+		{locationMarker}
+		</GoogleMap>
+    </div>
 	) : <>Loading...</>
 }
 
@@ -133,12 +184,14 @@ const infoWindowStyle = {
   
   //Map center location
 
-  const center = {
-	lat:  45.529819917244254,
-	lng: -73.60361034602055,
-  }
-//melbourne
-// const center: google.maps.LatLngLiteral = {
-// 	lat: -37.8136,
-// 	lng: 144.9631
+//   const center = {
+// 	lat:  45.529819917244254,
+// 	lng: -73.60361034602055,
 //   }
+
+//melbourne
+
+const center: google.maps.LatLngLiteral = {
+	lat: -37.8136,
+	lng: 144.9631
+  }
--- components/mapcontainer.tsx
+++ components/mapcontainer.tsx
@@ -1,9 +1,72 @@
 import {promises as fs} from 'fs';
 import Map from './map';
+import {ReactNode, ReactElement} from 'react'
+import { Polygon, PolygonProps, Polyline } from '@react-google-maps/api';
+import { Position, LineString } from 'geojson';
 
 export default async function MapContainer() {
 	const regionData = await fs.readFile(process.cwd() + '/app/data/melbourne.geojson', 'utf8');
+	const regionDataObj: GeoJSON.FeatureCollection = JSON.parse(regionData);
+	const regionLines = makeLines(regionDataObj);
+	const regionPolygons = makePolygons(regionDataObj);
+	type featureData = {
+		featureName : string,
+		coords: { lat: number; lng: number; }[]
+	};
 	return(
-		<Map regionData = {regionData} />
+		<Map regionData = {regionData} regions = {regionPolygons} lines = {regionLines}/>
 	)
+		interface PolygonFeature extends GeoJSON.Feature {
+			geometry: GeoJSON.Polygon;
+			properties: GeoJSON.GeoJsonProperties & {Name: string};
+		}
+		//Make the polygons
+		function makePolygons(regionDataObj: GeoJSON.FeatureCollection) {
+
+			//filter to polygons
+			const isPolygon = (region: GeoJSON.Feature) => region.geometry.type === "Polygon";
+			const polygons : GeoJSON.Feature[] = regionDataObj.features.filter(isPolygon);
+			//filter to polygons with valid properties and coordinates
+			const isValidPolygon = (region: GeoJSON.Feature): region is PolygonFeature => {return 'properties' in region && 'geometry' in region && 'coordinates' in region.geometry && region.geometry.coordinates.length > 0;}
+			const validPolygons = polygons.filter(isValidPolygon);
+			//Create the regions
+			const regionPolygons = validPolygons.map((region: PolygonFeature) => {
+				const regionName: string = region.properties.Name;
+				//convert coords to latlong (for some reason polygon has an extra array layer than polyline)
+				const regionCoords = region.geometry.coordinates[0].map((coord: Position) => {
+					const latlong = coord as number[];
+					return {lat: latlong[1], lng: latlong[0]}
+				})
+				return {featureName: regionName, coords: regionCoords};
+			})
+			
+			return regionPolygons;
+	}
+
+		interface LineFeature extends GeoJSON.Feature {
+			geometry: LineString;
+			properties: GeoJSON.GeoJsonProperties & {Name: string};
+		}
+	
+		function makeLines(regionDataObj: GeoJSON.FeatureCollection) {
+			//filter to polylines
+			const isPolyLine = (line: GeoJSON.Feature) => line.geometry.type === "LineString";
+			const polyLines : GeoJSON.Feature[] = regionDataObj.features.filter(isPolyLine);
+
+			//filter to polylines with valid properties and coordinates
+			const isValidPolyLine = (line: GeoJSON.Feature): line is LineFeature => {return 'properties' in line && 'geometry' in line && 'coordinates' in line.geometry && line.geometry.coordinates.length > 0;}
+			const validPolyLines = polyLines.filter(isValidPolyLine);
+
+			//Create the lines
+			const regionLines = validPolyLines.map((line: LineFeature) => {
+				const lineName: string = line.properties.Name
+				//convert coords to latlong
+				const lineCoords = line.geometry.coordinates.map((coord: Position) => {
+					const latlong = coord as number[];
+					return {lat: latlong[1], lng: latlong[0]}
+				})
+				return {featureName: lineName, coords: lineCoords};
+			})
+			return regionLines;
+		}
 }
