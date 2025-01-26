896c276e5a9534c9bd54a9d921fe637056d7382f
michael <Michaelturvey97@gmail.com>
2025-01-26 17:14:00 -0500

    styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import MapContainer from "../components/mapcontainer";
+import { Button, Title, Flex} from '@mantine/core';
+import MapContainer from '@components/mapcontainer';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-        <MapContainer />
+    <Flex direction="column" align="center" justify="center" style={{height: "100vh"}}>
+      <Title>Metro Game</Title>
+      <Button component={Link} href="/game">Start Game</Button>
+      <MapContainer />
+
+    </Flex>
   );
 }
