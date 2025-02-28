3f2c49c1dafcbbc69301dfd2a97adbeb2ee59151
michael <Michaelturvey97@gmail.com>
2025-02-28 12:15:50 -0500

    client state in session

--- components/ClientContainer.tsx
+++ components/ClientContainer.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { MetroMayhem } from "@/scripts/Game";
-import { createContext, useEffect, useRef, useState } from "react";
+import { createContext, use, useEffect, useRef, useState } from "react";
 import { Client } from "boardgame.io/react";
 import { SocketIO } from "boardgame.io/multiplayer";
 import type { ClientSetupData, GameSetupData, MetroGameBoardProps, PlayerData } from "@/scripts/types";
 import JoinGameLobby from "./lobby/JoinGame";
 import { LobbyClient } from "boardgame.io/client";
 import { Center, Stack, Text } from "@mantine/core";
+import { set } from "lodash";
 
 export const GameContext = createContext({} as MetroGameBoardProps);
 
@@ -14,16 +15,41 @@ export default function ClientContainer(
 	props: GameSetupData & { children: React.ReactNode }
 ) {
 	const [initialPlayerData, setPlayerData] = useState<PlayerData>();
+	const [busy, setBusy] = useState(true);
+	
+	useEffect(() => {
+		const sessionPlayerData = sessionStorage.getItem("sessionPlayerData");
+		if (sessionPlayerData){
+			const loadedPlayerData = JSON.parse(sessionPlayerData) as PlayerData;
+			console.log("setting player data from session storage", sessionPlayerData);
+			setPlayerData(loadedPlayerData);
+		}
+		setBusy(false);
+	}, []);
+
 	const GameClient = Client({
 		game: MetroMayhem(props.zonePolygons),
 		board: AppAsBoardgame,
-		debug: true,
+		debug: {
+			collapseOnLoad: true,
+		},
 		numPlayers: 10,
 		multiplayer: SocketIO({
 			server: "localhost:8000",
 		}),
 	}) as React.JSXElementConstructor<ClientSetupData>
 	
+	if (busy){
+		return (
+			<Center>
+				<Stack>
+					<h1>Untitled City Game</h1>
+					<p>Loading...</p>
+				</Stack>
+			</Center>
+		)
+	}
+
 	if (initialPlayerData){
 		return (
 			<GameClient initialPlayerData={initialPlayerData} playerID={initialPlayerData.playerID} credentials = {initialPlayerData.playerCredentials} {...props} />
@@ -44,6 +70,7 @@ export default function ClientContainer(
 function AppAsBoardgame(props: MetroGameBoardProps) {
 	const { children, ...rest } = props;
 	const { moves, playerID, initialPlayerData } = props;
+	sessionStorage.setItem("sessionPlayerData", JSON.stringify(initialPlayerData));
 	const triedConnect = useRef(false)
 	useEffect(() => {
 		if(!triedConnect.current && playerID && !props.G.allPlayersData[playerID]){
@@ -58,10 +85,6 @@ function AppAsBoardgame(props: MetroGameBoardProps) {
 	}
 	return (
 		<GameContext.Provider value={{ ...rest }}>
-			<div style={{position: "fixed"}} >
-				<p>Hello I am the game board</p>
-				<p>Player: {playerID}</p>
-			</div>
 			{children}
 		</GameContext.Provider>
 	);
